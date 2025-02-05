0dd53c3c7dbaf28cb027e6c648ef5423a2f4d6fe
Kim Daecheol <kdc9619@naver.com>
2025-02-05 16:35:17 +0900

    feat: 프로젝트 셋팅

---
+++ src/main/java/com/ssginc/threadconnect/ThreadConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ssginc.threadconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadConnectApplication.class, args);
+    }
+
+}
