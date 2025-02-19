4de82d1091fee81e5c036afa8ea907de837647d9
ChangHoon <nick6253@naver.com>
2025-02-19 21:04:18 +0900

    Chore: 프로젝트 기초 세팅
    - 프로젝트 디렉토리 구조 세팅
    - build.gradle dependency 추가
    - entity 설정

--- src/test/java/com/ssginc/showping/ShowPingApplicationTests.java
+++ src/test/java/com/ssginc/showping/ShowPingApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ThreadConnectApplicationTests {
+class ShowPingApplicationTests {
 
     @Test
     void contextLoads() {
