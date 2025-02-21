55aa6d488e45b89d4856debb8195f4214995c8a9
Kim Daecheol <kdc9619@naver.com>
2025-02-21 17:40:08 +0900

    Feat: VOD 정보 DTO 클래스 정의
    - VOD 목록 확인을 위한 DTO 클래스를 정의

---
+++ src/main/java/com/ssginc/showping/dto/response/VodResponseDto.java
@@ -0,0 +1,30 @@
+package com.ssginc.showping.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+/**
+ * VOD 응답정보를 보내주기 위해 정의한 DTO 클래스
+ * <p>
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VodResponseDto {
+
+    private Long streamNo;                  // 영상 번호
+    private String streamTitle;             // 영상 제목
+    private String productName;             // 상품 이름
+    private Long productPrice;              // 상품 가격
+    private int productSale;                // 상품 할인율
+    private String productImg;              // 상품 이미지
+    private LocalDateTime streamStartTime;  // 영상 시작 시간
+    private LocalDateTime streamEndTime;    // 영상 종료 시간
+
+}
