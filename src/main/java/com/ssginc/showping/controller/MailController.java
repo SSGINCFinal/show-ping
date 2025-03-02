b9f73c44dc7b9351ee8c9544a72fda3af0b8d231
ParkHeonWoo <phwoo1315@naver.com>
2025-03-03 00:16:48 +0900

    Feat: 회원가입 과정에서 SMTP 이메일 인증 기능 추가 #68

---
+++ src/main/java/com/ssginc/showping/controller/MailController.java
@@ -0,0 +1,26 @@
+package com.ssginc.showping.controller;
+
+import com.ssginc.showping.dto.object.MailDto;
+import com.ssginc.showping.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/signup")
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+    // 이메일 인증 코드 보내기
+    @PostMapping("/send-code")
+    public String sendCode(@RequestBody MailDto mailDto) {
+        return mailService.sendVerificationCode(mailDto.getEmail());
+    }
+
+    // 인증 코드 확인하기
+    @PostMapping("/verify-code")
+    public boolean verifyCode(@RequestParam String email, @RequestParam String code) {
+        return mailService.verifyCode(email, code);
+    }
+}
---
+++ src/main/java/com/ssginc/showping/dto/object/MailDto.java
@@ -0,0 +1,12 @@
+package com.ssginc.showping.dto.object;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class MailDto {
+    private String email;
+}
